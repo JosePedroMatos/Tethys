d6e5be2cf8bb923af21efae98bc12ec14cfaf88a
JosePedroMatos <jpgscm@gmail.com>
2017-05-19 09:34:55 +0200

    Listen to external requests on port 8000

--- tethys.sh
+++ tethys.sh
@@ -5,6 +5,6 @@
 (xterm -title "Celery beat" -hold -e celery -A tethys beat -l info &);
 (xterm -title "Celery download" -hold -e celery -A tethys worker -Q download -n download -l info --pool=solo &);
 (xterm -title "Celery train" -hold -e celery -A tethys worker -Q train -n train -l info --pool=solo &);
-(xterm -title django -hold -e python manage.py runserver)
+(xterm -title django -hold -e python manage.py runserver 0.0.0.0:8000)
 ) | parallel
 exit 0
